642dec03135b110d4eb9ab3d746b6a6b66a8b108
rahulpanwar-tm <rahul.panwar@teramatrix.in>
2020-03-17 19:00:35 +0530

    update all

--- jenkins/backup_script/mysql-import.sh
+++ jenkins/backup_script/mysql-import.sh
@@ -3,6 +3,20 @@ VAR=$(ls -tr|tail -1)
 ## declare an array variable
 declare -a arr=("$1" "$2")
 
+delete_ary_elmt() {
+  local word=$1      # the element to search for & delete
+  local aryref="$2[@]" # a necessary step since '${!$2[@]}' is a syntax error
+  local arycopy=("${!aryref}") # create a copy of the input array
+  local status=1
+  for (( i = ${#arycopy[@]} - 1; i >= 0; i-- )); do # iterate over indices backwards
+    elmt=${arycopy[$i]}
+    [[ $elmt == $word ]] && unset "$2[$i]" && status=0 # unset matching elmts in orig. ary
+  done
+  return $status # return 0 if something was deleted; 1 if not
+}
+
+delete_ary_elmt "no_database" arr
+
 ## now loop through the above array
 for i in "${arr[@]}"
 do
